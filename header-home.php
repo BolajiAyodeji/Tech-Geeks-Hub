7135aa69d9f67be37d498228f84438b46f2fcf35
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-05-04 15:01:45 +0100

    Add files via upload

---
+++ header-home.php
@@ -0,0 +1,78 @@
+<DOCTYPE html>
+    <html lang="en-US">
+
+    <head>
+      <title> Tech Geeks Hub</title>
+        <!-- Required Meta Tags-->
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" , initial-scale="1.0" shrink-to-fit="no">
+
+            <!-- Style Sheets-->
+
+            <link rel="stylesheet " href="https://fonts.googleapis.com/css?family=Montserrat ">
+            <link rel="stylesheet " href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css ">
+
+            <!-- Bootstrap CDN -->
+
+            <link rel="stylesheet " href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css
+            " integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4 " crossorigin="anonymous ">
+</head>
+<header>
+            <!-- Nav Bar -->
+          
+            <nav class="navbar navbar-expand-lg navbar-light bg-success">
+                    <img src="TGHLogo.jpg" style="width:15%" class="btn btn-outline-light bg-success">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                      aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                          <a class="nav-link" href="index.php">Home</a>
+                            <span class="sr-only">(current)</span>
+                          </a>
+                        </li>
+                        <div class="dropdown-divider"></div>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            Explore
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#about">Community</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#about">Events</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#founder">Meet Ups</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#contact">Partners</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#founder">Subscribe</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#founder">Contact Us</a>
+                           </div>
+                        </li>
+                        <div class="dropdown-divider"></div>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#blog">Blog</a>
+                              </li>
+                        <div class="dropdown-divider"></div>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#app">The App</a>
+                        </li>
+                        <div class="dropdown-divider"></div>
+                      </ul>
+                      <form class="form-inline my-1 ">
+                        <input class="form-control mr-sm-1" type="email" placeholder="Email" name="email" required>
+                        <br /><br />
+                        <input class="form-control mr-sm-2" type="password" placeholder="Password" name="pwd" required>
+                        <button class="btn btn-outline-light" type="submit">Login&nbsp<i class="fa fa-user"></i></button>&nbsp&nbsp
+                        <a class="btn btn-outline-light" href="signup.php">Sign Up&nbsp<i class="fa fa-user-plus"></i></a>
+                      </form>
+                    </div>
+                  </nav>
+                  
+</header>
---
+++ header-login.php
@@ -0,0 +1,75 @@
+<DOCTYPE html>
+        <html lang="en-US">
+    
+        <head>
+          <title> LOGIN | Tech Geeks Blog </title>
+            <!-- Required Meta Tags-->
+    
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width" , initial-scale="1.0" shrink-to-fit="no">
+    
+                <!-- Style Sheets-->
+    
+                <link rel="stylesheet " href="https://fonts.googleapis.com/css?family=Montserrat ">
+                <link rel="stylesheet " href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css ">
+    
+                <!-- Bootstrap CDN -->
+    
+                <link rel="stylesheet " href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css
+                " integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4 " crossorigin="anonymous ">
+    </head>
+    <header>
+                <!-- Nav Bar -->
+              
+                <nav class="navbar navbar-expand-lg navbar-light bg-success">
+                    <img src="TGHLogo.jpg" style="width:15%" class="btn btn-outline-light bg-success">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                      aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                          <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                              <a class="nav-link" href="index.php">Home
+                                <span class="sr-only">(current)</span>
+                              </a>
+                            </li>
+                            <div class="dropdown-divider"></div>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            Explore
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#about">Community</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#about">Events</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#founder">Meet Ups</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#contact">Partners</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#founder">Subscribe</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#founder">Contact Us</a>
+                           </div>
+                        </li>
+                        <div class="dropdown-divider"></div>
+                                <li class="nav-item">
+                                <a class="nav-link" href="#blog">Blog</a>
+                              </li>
+                        <div class="dropdown-divider"></div>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#app">The App</a>
+                        </li>
+                        <div class="dropdown-divider"></div>
+                      </ul>
+                          <form class="form-inline my-1 ">
+                            
+                            <a class="btn btn-outline-light" href="signup.php">Don't have an account? <i class="fa fa-sign-in"></i> </a>
+                          </form>
+                        </div>
+                      </nav>
+    
+    </header>
